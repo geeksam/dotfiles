6b62bdbf1fd6f9f60d500a99e2a5f601752c89c9
Sam Livingston-Gray <geeksam@gmail.com>
2024-03-04 11:35:36 -0800

    hammerspoon config

--- hammerspoon-init.lua
+++ hammerspoon-init.lua
@@ -1,17 +1,44 @@
+--[[
+
+Hammerspoon docs:  http://www.hammerspoon.org/docs/
+Hammerspoon 'Getting Started' guide:  https://www.hammerspoon.org/go/
+
+A few quick snippets:
+
+  Hammerspoon popup alert:
+    hs.alert.show("Hello, world!")
+
+  MacOS notification:
+    hs.notify.new({title="Hammerspoon", informativeText="Hello, world!"}):send()
+
+  Using a spoon:
+    hs.loadSpoon("AClock")
+    spoon.AClock:init()
+    hs.hotkey.bind(meh, "c", function() spoon.AClock:toggleShow() end)
+
+TODO list:
+
+* turn bluetooth back on (e.g., after sleep)
+  * https://github.com/toy/blueutil
+
+* consider moving window management out of Moom and into here
+  * http://www.hammerspoon.org/docs/hs.screen.html
+  * https://livingissodear.com/posts/using-hammerspoon-for-window-management/
+
+--]]
+
 meh   = {"ctrl", "alt", "shift"}
 mash  = {"ctrl", "alt", "cmd"}
 hyper = {"ctrl", "alt", "shift", "cmd"}
 
-function focus_app(app_identifier)
+
+
+function focus_app(app_identifier, all_windows)
   return function()
     hs.application.launchOrFocus(app_identifier)
-  end
-end
-
-function open_folder(path)
-  return function()
-    local _cmd = "open " .. path
-    hs.execute(_cmd)
+    if all_windows then
+      hs.application(app_identifier):selectMenuItem { "Window", "Bring All to Front" }
+    end
   end
 end
 
@@ -22,6 +49,22 @@ function hermes_cmd(cmd)
   end
 end
 
+function open_folder(path)
+  return function()
+    local _cmd = "open " .. path
+    hs.execute(_cmd)
+  end
+end
+
+function shell_cmd(cmd)
+  return function()
+    hs.alert.show(cmd)
+    hs.execute(cmd)
+  end
+end
+
+
+
 -- Pandora/Hermes
 -- On the Mac KB, F7=rewind, F8=play/pause, F9=next
 -- Since Pandora doesn't do rewind, F7 seems appropriate for activating the app
@@ -30,34 +73,28 @@ hs.hotkey.bind( meh, "F7", focus_app("Hermes") )
 hs.hotkey.bind( meh, "F8", hermes_cmd("playpause") )
 hs.hotkey.bind( meh, "F9", hermes_cmd("next song") )
 
--- GIFs
-hs.hotkey.bind( meh, "g", open_folder("~/Dropbox/GIFs") )
+-- turn bluetooth on (necessitated by work security policy)
+-- (doesn't actually work tho?)
+-- hs.hotkey.bind( meh, "a", shell_cmd("blueutil -p 0") )
+-- hs.hotkey.bind( meh, "b", shell_cmd("blueutil -p 1") )
 
--- Apps
 hs.hotkey.bind( meh, "c", focus_app("Google Chrome") )
 hs.hotkey.bind( meh, "d", focus_app("Dash") )
 hs.hotkey.bind( meh, "f", focus_app("Finder") )
+hs.hotkey.bind( meh, "g", open_folder("~/Dropbox/GIFs") )
 hs.hotkey.bind( meh, "h", focus_app("Hammerspoon") )
 hs.hotkey.bind( meh, "k", focus_app("Ergodox EZ Configurator") )
 hs.hotkey.bind( meh, "m", focus_app("TextMate") )
-hs.hotkey.bind( meh, "s", focus_app("Slack") )
+hs.hotkey.bind( meh, "p", focus_app("1Password") )
+hs.hotkey.bind( meh, "s", focus_app("Slack", true) )
 hs.hotkey.bind( meh, "t", focus_app("iTerm") )
-hs.hotkey.bind( meh, "v", focus_app("MacVim") )          -- TODO: make this cycle instances if active
+hs.hotkey.bind( meh, "v", focus_app("MacVim") ) -- TODO: cycle instances? https://www.hammerspoon.org/Spoons/AppWindowSwitcher.html
+hs.hotkey.bind( meh, "w", focus_app("Safari") )
 hs.hotkey.bind( meh, "x", focus_app("Camera Settings") ) -- X is next to Z?  ¯\_(ツ)_/¯
-hs.hotkey.bind( meh, "z", focus_app("Zoom.us") )
+hs.hotkey.bind( meh, "z", focus_app("Zoom.us", true) )
 
 
 
--- from the 'getting started' documentation:
---
--- hs.hotkey.bind(meh, "S", function()
---   hs.alert.show("Hello, world!")
---   hs.notify.new({title="Hammerspoon", informativeText="Hello, world!"}):send()
--- end)
---
--- hs.loadSpoon("AClock")
--- spoon.AClock:init()
--- hs.hotkey.bind(meh, "C", function()
---   spoon.AClock:toggleShow()
--- end)
-
+-- preload a few things so they don't clutter up the console when a hotkey is invoked
+hs.application()
+hs.window()
